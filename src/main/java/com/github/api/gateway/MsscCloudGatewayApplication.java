2ed47778f48d5768a0e27f5ab4869a1593aae470
murilobauerc <murilobauerc@gmail.com>
2021-07-04 05:01:56 -0300

    adding foundation for a reactive gateway project

---
+++ src/main/java/com/github/api/gateway/MsscCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.github.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscCloudGatewayApplication.class, args);
+	}
+
+}
